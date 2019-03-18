2b988970cef52cdb47fe650c7277bb466494beca
dothienphuc17892@gmail.com <dothienphuc17892@gmail.com>
2019-03-18 08:57:21 +0700

    Test

---
+++ webapp/controller/Categories.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("DemoBOPF_Product.controller.Categories", {
+		onInit: function(){
+			var view = this.getView();
+"test" 
+		}
+	});
+});
